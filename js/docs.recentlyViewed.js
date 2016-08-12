d217582098957f881ee7be4debb636f4c290ec89
Kevin Ball <kmball11@gmail.com>
2016-08-12 11:13:38 -0700

    Get recently viewed pages recording

---
+++ js/docs.recentlyViewed.js
@@ -0,0 +1,69 @@
+if (typeof(window.FoundationDocs) === 'undefined') { window.FoundationDocs = {}; }
+if (typeof(window.FoundationDocs.RecentViews) === 'undefined') { window.FoundationDocs.RecentViews = {}; }
+
+var FoundationDocs = window.FoundationDocs;
+!(function() {
+  var storageName = 'FOUNDATION_RECENT_VIEWS';
+  var maxLength = 5;
+
+  FoundationDocs.RecentViews.storeViews = function(views) {
+    localStorage.setItem(storageName, JSON.stringify(views));
+    return views;
+  };
+
+  FoundationDocs.RecentViews.addCurrent = function() {
+    var name = $('h1').text();
+    var url = window.location.href;
+    FoundationDocs.RecentViews.clearView(name);
+    var views = FoundationDocs.RecentViews.getViews();
+    views.unshift({name: name, url: url})
+    views.slice(maxLength - 1, 1)
+    FoundationDocs.RecentViews.storeViews(views);
+    return;
+  }
+
+  FoundationDocs.RecentViews.getViews = function() {
+    var viewStr = localStorage.getItem(storageName);
+    if (viewStr) {
+      return JSON.parse(viewStr);
+    } else {
+      return [];
+    }
+  };
+
+  // Find view with name, remove it, return true if
+  // found and removed, false otherwise.
+  FoundationDocs.RecentViews.clearView = function(name) {
+    var views = FoundationDocs.RecentViews.getViews();
+    var found = -1;
+    for(var i = 0; i < views.length; i++) {
+      if(views[i].name === name) {
+        found = i;
+        break;
+      }
+    }
+    if(found > -1) {
+      views.splice(i, 1);
+      FoundationDocs.RecentViews.storeViews(views);
+      return true;
+    }
+    return false;
+  };
+
+  FoundationDocs.RecentViews.clearViews = function() {
+    FoundationDocs.RecentViews.storeViews([]);
+  };
+
+  FoundationDocs.RecentViews.updateViewsList = function() {
+    var views = FoundationDocs.RecentViews.getViews();
+    var viewStr = ''
+    for(var i = 0; i < views.length; i++) {
+      viewStr = viewStr + "<li class='recently-viewed-item'><a href='" + views[i].url + "'>" + views[i].name + "</a></li>";
+    }
+    $('[data-recently-viewed]').html(viewStr);
+  };
+
+})();
+
+FoundationDocs.RecentViews.addCurrent();
+FoundationDocs.RecentViews.updateViewsList();
